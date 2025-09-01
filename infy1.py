d66c88a6146a29ff87be306ec8563ef8c7ce0de4
Saif Ahmed <saif.r.ahmed@gmail.com>
2025-09-01 19:34:23 -0400

    cleanup

--- infy1.py
+++ infy1.py
@@ -1,16 +1,19 @@
 import math
 
+STEPS_TO_RUN = 10000000000
 
-series_sum = 0
-for s in range(1, 10000000000, 4):
-    
-    term = 1/s
-    next_term = 1/(s+2)
-    series_sum += term
-    series_sum -= next_term
-    approx = series_sum*4
-    diff = abs(approx - math.pi)
-    if (s-1)%100000 == 0:
-        print(f"On step {s:,} we're adding 1/{s} and subtracting 1/{s+2} for approximation {approx:0.15f} and difference {diff:0.15f}")
-    
-print(series_sum)
+def main():
+    series_sum = 0
+    for s in range(1, STEPS_TO_RUN, 4):
+        
+        term = 1/s
+        next_term = 1/(s+2)
+        series_sum += term
+        series_sum -= next_term
+        approx = series_sum*4
+        diff = abs(approx - math.pi)
+        if (s-1)%100000 == 0:
+            print(f"On step {s:,} we're adding 1/{s} and subtracting 1/{s+2} for approximation {approx:0.15f} and difference {diff:0.15f}")
+        
+if __name__ == "__main__":
+    main()
